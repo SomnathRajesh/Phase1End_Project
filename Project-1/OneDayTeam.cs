6b190247bc07726d95d4fb930bd83d7dc5b0c0af
Somnath Rajesh <somnathpratik830@gmail.com>
2023-08-22 17:30:25 +0530

    Add project files.

---
+++ Project-1/OneDayTeam.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_1
+{
+    internal class OneDayTeam : ITeam
+    {
+        Player pId,pN;
+        int count=0;
+        public static List<Player> oneDayTeam = new List<Player>();
+        private int _capacity;
+        public OneDayTeam() 
+        {
+            _capacity = 11;
+        }
+
+        public void Add(Player player)
+        {
+            if(count == _capacity) 
+            {
+                Console.WriteLine("Sorry you can't add players more than 11");
+            }
+            else
+            {
+                oneDayTeam.Add(player);
+                count += 1;
+
+            }
+        }
+
+        public List<Player> GetAllPlayers()
+        {
+            return oneDayTeam;
+        }
+
+        public Player GetPlayerById(int playerId)
+        {
+                foreach (Player player in oneDayTeam)
+                {
+                    if (player.PlayerId == playerId)
+                    {
+                        pId = player;
+                    }
+                }
+                return pId;
+            
+            
+            
+        }
+
+        public Player GetPlayerByName(string playerName)
+        {
+            foreach(Player player in oneDayTeam)
+            {
+                if(player.PlayerName == playerName)
+                {
+                    pN = player;
+                }
+            }
+            return pN;
+        }
+
+        public void Remove(int playerId)
+        {
+            int i;
+            //foreach(Player player in oneDayTeam)
+            //{
+            //    if(player.PlayerId == playerId)
+            //    {
+            //        oneDayTeam.Remove(player);
+            //    }
+            //}
+            for(i=count-1;i>=0;i--)
+            {
+                if (oneDayTeam[i].PlayerId == playerId)
+                {
+                    oneDayTeam.RemoveAt(i);
+                    count -= 1;
+                }
+            }
+            //if(count == 0)
+            //{
+            //    Console.WriteLine("No player to remove");
+            //}
+        }
+    }
+}
---
+++ Project-1/Program.cs
@@ -0,0 +1,82 @@
+﻿using Project_1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string op;
+        OneDayTeam team = new OneDayTeam();
+        do
+        {
+            Console.WriteLine("Enter 1:To Add Player 2:To Remove Player by Id 3.Get Player By Id 4.Get Player by Name 5.Get All Players:");
+            int choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+
+                case 1:
+                    Player player = new Player();
+                    Console.WriteLine("Enter Player Id:");
+                    player.PlayerId = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter Player Name");
+                    player.PlayerName = Console.ReadLine();
+                    Console.WriteLine("Enter Player Age");
+                    player.PlayerAge = int.Parse(Console.ReadLine());
+                    team.Add(player);
+                    break;
+                case 2:
+                    Console.WriteLine("Enter Player Id to Remove:");
+                    int id = int.Parse(Console.ReadLine());
+                    team.Remove(id);
+                    Console.WriteLine("Player is removed successfully");
+                    break;
+                case 3:
+                    Console.WriteLine("Enter Player Id:");
+                    int i = int.Parse(Console.ReadLine());
+                    Player pdId = team.GetPlayerById(i);
+                    if(pdId != null)
+                    {
+                        Console.WriteLine(pdId.PlayerId + " " + pdId.PlayerName + " " + pdId.PlayerAge);
+                    }
+                    else
+                    {
+                        Console.WriteLine("There is no player with that Id!!");
+                    }
+                    
+                    break;
+                case 4:
+                    Console.WriteLine("Enter Player Name:");
+                    string name = Console.ReadLine();
+                    Player pdN = team.GetPlayerByName(name);
+                    if(pdN != null)
+                    {
+                        Console.WriteLine(pdN.PlayerId + " " + pdN.PlayerName + " " + pdN.PlayerAge);
+                    }
+                    else
+                    {
+                        Console.WriteLine("There is no player with that Name!!");
+                    }
+                    break;
+                case 5:
+                    List<Player> list = team.GetAllPlayers();
+                    if(list.Count != 0)
+                    {
+                        foreach (Player p in list)
+                        {
+                            Console.WriteLine(p.PlayerId + " " + p.PlayerName + " " + p.PlayerAge);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No players in the team!!!");
+                    }
+                    
+                    break;
+                default: 
+                    Console.WriteLine("Wrong option!!");
+                    break;
+            }
+            Console.WriteLine("Do you want to continue (yes/no)?:");
+            op = Console.ReadLine();
+        } while (op == "yes");
+    }
+}
